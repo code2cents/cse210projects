b6c3e9737a2f536e6eaaa3bf999de3b5bea1b7e1
code2cents <ur2santo@gmail.com>
2023-09-13 22:14:33 -0300

    Basic prep 1 and 2 - week 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string firstname = Console.ReadLine();
+        Console.WriteLine("What is your second name?");
+        string secondname = Console.ReadLine();
+        Console.WriteLine($"Your name is {firstname} {secondname}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade %");
+        string gradeInput = Console.ReadLine();
+        int valueUser = int.Parse(gradeInput);
+        int a = 90;
+        int b = 80;
+        int c = 70;
+        int d = 60;
+        
+
+        if(valueUser >= a && valueUser < 100)
+        {
+            Console.WriteLine("Your grade is A,you passed the course!");
+        }
+        else if(valueUser >= b && valueUser < a )
+        {
+            Console.WriteLine("Your grade is B,you passed the course!");
+        }
+        else if(valueUser >= c && valueUser < b)
+        {
+            Console.WriteLine("Your grade is C,you passed the course!");
+        }
+        else if(valueUser >= d && valueUser < c)
+        {
+            Console.WriteLine("Your grade is D,you failed the course,try again.");
+        }
+        else if(valueUser >= 101)
+        {
+            Console.WriteLine("Error,enter a valid grade.");
+        }
+        else{
+            Console.WriteLine("Your grade is F,you failed the course,try again.");
+        }
     }
 }
